467aac2f4dbc5834d25c52052ef5393180a5e17e
unknown <argiebaroy@yahoo.com>
2026-01-16 16:59:26 +0800

    review

---
+++ review.py
@@ -0,0 +1,58 @@
+'''
+x=3
+y=5
+print(x/y)
+print(x+y)
+print(x-y)
+print(x*y)
+print(x//y)
+print(x%y)
+print(y**x)
+'''
+
+
+'''
+#convertion of data types
+x="10"
+y=10
+print(y + int(x))
+from string to integer
+'''
+''''
+def greet(greetings:str, name:str) -> None:
+    print(f"{greetings}, {name}")
+
+greet("hello", "bince")
+'''
+'''
+def add(a:str, b:str) -> str:
+    print(f"i have a {b}, I have an {a}")
+    return a+b
+
+print(add("apple", "pen"))
+'''
+'''
+for v in range(5):
+    print("hoy")
+'''
+'''
+names = ["vince","bins", "nibs"]
+for v in names:
+    print(f"hi, {v}")
+i = 0
+while i < 5:
+    print(i)
+    i+=1
+'''
+'''
+while True:
+ user_input = input('You:')
+
+ if user_input == "kumain ka na?":
+    print('baby:di pa po')
+ elif user_input == "ano ulam mo?":
+    print('baby: hakdog')
+ else:
+    print('baby:pinagsasabi mo?')
+'''
+
